4e058bbe91f2ac3a732b74a73937cb1af71651ad
Vikrant <06learntocode11@gmial.com>
2025-03-21 13:28:55 +0530

    github data added

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-require('dotenv').config;
-// import express from 'express';
+require('dotenv').config();
+// import 'dotenv/config';
+// import exp`ress from 'express';
 const express = require('express');
 const app = express();
 const port = 3000;
+const githubData = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Molecule Man",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      },
+      {
+        "name": "Madame Uppercut",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Eternal Flame",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      }
+    ]
+  }
+  
 
 app.get('/', (req, res) => {
     res.send('hello world!')
@@ -20,6 +59,10 @@ app.get('/yt', (req, res) => {
     res.send('<h3>Featuring animes on yt with latest updates! </h3>')
 })
 
-app.listen(process.env.PORT, () => {
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
+
+app.listen(process.env, () => {
     console.log(`example app listening on port ${port}`);
 })
